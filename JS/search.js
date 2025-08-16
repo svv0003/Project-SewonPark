505f2ef6f722cb0ac1732bf373ec719ec0edfd89
svv0003 <ssetoo4689@naver.com>
2025-08-16 12:44:05 +0900

    검색 결과 페이지 모바일 css

--- JS/search.js
+++ JS/search.js
@@ -68,9 +68,11 @@ $(function () {
             `
             <div class="product-item" onclick="goToDetail('${product.referenceNumber}')" >
               <img src="${product.image[initialColor]}" alt="${product.title} - ${initialColor}">
-              <p class="info4">${product.brand}</p>
-              <p class="info2">${product.title}</p>
-              <p class="info1">${product.price.toLocaleString()}원</p>
+              <div class="product-info">
+                <p class="info4">${product.brand}</p>
+                <p class="info2">${product.title}</p>
+                <p class="info1">${product.price.toLocaleString()}원</p>
+              </div>
             </div>
             `
           );
